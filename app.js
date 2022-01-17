3c04fe6a7e1198417c3359920681fa00e1a934c6
Muzahidul Islam <86096333+teeshir@users.noreply.github.com>
2022-01-17 20:39:29 +0600

    test 1 ok everythings is running ok -------

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 var wontime=0;
 var losetime=0;
-for (let i = 0; i < 6; i++) {
-    
-    var inputNumber=parseInt(prompt("enter a number 1 to 5 :"));
 
-var rendom=Math.floor(Math.random()*5)+1; 
+var n=parseInt(prompt("How many turn you want to play? :"))
+for (let i = 1; i <=n; i++) {
+    
+    var inputNumber=parseInt(prompt( "Turn" + " "+ i + "  :  "+ "enter a number 1 to" + n) );
+
+var rendom=Math.floor(Math.random()*n)+1; 
  if (inputNumber===rendom) {
-    document.write("You have won ! " + "<br>")
+    document.write("You have won ! " + "<br>" + "<br>")
 
      wontime++;
  }
  else{
-    document.write("You have lose rendom number was"  + rendom  + "<br>");
+    document.write("You have lose rendom number was"  + rendom  + "<br>" );
      losetime++
  }
 }
 
-document.write("You won"+ wontime +"time" + "<br>");
-document.write("You lose"+ losetime +"time");
+document.write("You won" +  "  "+ wontime + "  " + "time" + "<br>" );
+document.write("You lose" + "  "+ losetime + "  " + "time");
+// almost onkey
